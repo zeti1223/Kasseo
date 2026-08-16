3222fd21634344d795fd104d445c8173de147fb8
Zeti_1223 <121749710+zeti1223@users.noreply.github.com>
2026-08-16 16:01:55 +0200

    Add editable port

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  base: '/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue()],
+    base: '/',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    server: {
+      port: Number(env.PORT) || 5173,
+      allowedHosts: true,
+    },
+  }
 })
