82cf75d6a25da6428ee505ff34d78a593d6e98fc
Zeti_1223 <121749710+zeti1223@users.noreply.github.com>
2026-08-20 18:51:24 +0200

    Remove total Fixes #43

--- src/components/charts/CategoryBreakdownChart.vue
+++ src/components/charts/CategoryBreakdownChart.vue
@@ -13,27 +13,11 @@ const hasExpenses = computed(() =>
   props.transactions.some((t) => t.type === "expense"),
 );
 const chartOptions = computed(() => radialOptions(props.currency));
-
-const total = computed(() =>
-  props.transactions
-    .filter((t) => t.type === "expense")
-    .reduce((s, t) => s + t.amount, 0),
-);
 </script>
 
 <template>
   <div style="height: 260px" class="relative">
     <Doughnut v-if="hasExpenses" :data="chartData" :options="chartOptions" />
-    <div
-      v-if="hasExpenses"
-      class="absolute inset-0 flex flex-col items-center justify-center pointer-events-none"
-      style="padding-bottom: 34px"
-    >
-      <span class="text-xs text-gray-500 dark:text-gray-400">{{ $t('common.total') }}</span>
-      <span class="text-sm font-semibold dark:text-white">
-        {{ total.toFixed(2) }} {{ currency }}
-      </span>
-    </div>
     <div
       v-else
       class="text-sm text-gray-500 dark:text-gray-400 flex items-center justify-center h-full"
